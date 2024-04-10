e1c72fbb36a27c751bc28b7e64938ef8a82bbcaf
Gahee Kim <soprue@naver.com>
2024-04-10 22:40:15 +0900

    Chore: eslint 및 prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['__tests__/**/*.js'],
+      rules: {
+        'max-depth': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'max-depth': ['error', 2],
+  },
+};
