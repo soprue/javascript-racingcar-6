28008ed2709f8d3e8237327c7fbaf993c0b5401d
Gahee Kim <soprue@naver.com>
2024-04-13 00:20:08 +0900

    Test: 자동차 이름 입력 중 쉼표만 있는 입력이나 공백 이름이 존재하는 경우

--- __tests__/input/inputCarNames.test.js
+++ __tests__/input/inputCarNames.test.js
@@ -1,8 +1,17 @@
 import validateCarNames from '../../src/validation/carNameValidator.js';
+import { InputError } from '../../src/errors/InputError.js';
+import { CAR_ERROR_MESSAGE } from '../../src/constants/error.js';
 
 describe('validateCarNames', () => {
   test('자동차 이름에 문제가 없는 경우 에러를 반환하지 않는다.', () => {
     const validInput = 'car1,car2,car3';
     expect(() => validateCarNames(validInput)).not.toThrow();
+  });
+
+  test('자동차 이름 입력 중 쉼표만 있는 입력이나 공백 이름이 존재하는 경우', () => {
+    const carNames = 'car1,,car2';
+
+    expect(() => validateCarNames(carNames)).toThrow(InputError);
+    expect(() => validateCarNames(carNames)).toThrow(CAR_ERROR_MESSAGE.EMPTY);
   });
 });
