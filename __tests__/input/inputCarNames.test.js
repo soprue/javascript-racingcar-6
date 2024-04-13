09dbe506a7b797257baa7d46af963eeb2b8d16be
Gahee Kim <soprue@naver.com>
2024-04-13 20:25:08 +0900

    Style: inputCarNames.test.js 변수 이름, 포맷팅 수정

--- __tests__/input/inputCarNames.test.js
+++ __tests__/input/inputCarNames.test.js
@@ -1,38 +1,35 @@
 import validateCarNames from '../../src/validation/carNameValidator.js';
-import { InputError } from '../../src/errors/InputError.js';
 import { CAR_ERROR_MESSAGE } from '../../src/constants/error.js';
 
-describe('validateCarNames', () => {
+describe('자동차 이름 유효성', () => {
   test('자동차 이름에 문제가 없는 경우 에러를 반환하지 않는다.', () => {
     const validInput = 'car1,car2,car3';
     expect(() => validateCarNames(validInput)).not.toThrow();
   });
 
   test('자동차 이름 입력 중 쉼표만 있는 입력이나 공백 이름이 존재하는 경우', () => {
-    const carNames = 'car1,,car2';
-
-    expect(() => validateCarNames(carNames)).toThrow(CAR_ERROR_MESSAGE.EMPTY);
+    const validInput = 'car1,,car2';
+    expect(() => validateCarNames(validInput)).toThrow(CAR_ERROR_MESSAGE.EMPTY);
   });
 
   test('자동차 이름이 2대 이상 입력되었는지 확인', () => {
-    const carNames = 'car1';
-
-    expect(() => validateCarNames(carNames)).toThrow(
+    const validInput = 'car1';
+    expect(() => validateCarNames(validInput)).toThrow(
       CAR_ERROR_MESSAGE.MINIMUM_COUNT,
     );
   });
 
   test('자동차 이름이 중복 입력 된 경우', () => {
-    const carNames = 'car1,car1,car2';
-
-    expect(() => validateCarNames(carNames)).toThrow(
+    const validInput = 'car1,car1,car2';
+    expect(() => validateCarNames(validInput)).toThrow(
       CAR_ERROR_MESSAGE.DUPLICATION,
     );
   });
 
   test('자동차의 이름이 1자 이상 5자 이하인지 확인', () => {
-    const carNames = 'carcarcar,car1';
-
-    expect(() => validateCarNames(carNames)).toThrow(CAR_ERROR_MESSAGE.LENGTH);
+    const validInput = 'carcarcar,car1';
+    expect(() => validateCarNames(validInput)).toThrow(
+      CAR_ERROR_MESSAGE.LENGTH,
+    );
   });
 });
